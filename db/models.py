34f5b76057406b93c4e7ccd55cbea7cb006b04b8
Jefferson <alejorangel2008@gmail.com>
2025-05-19 06:52:48 -0500

    Subo base de datos adelanto

--- db/models.py
+++ db/models.py
@@ -49,3 +49,57 @@ class Clase(Base):
     
     aprendiz_id = Column(Integer, 
                           ForeignKey("aprendiz.id_Aprendiz"))
+    
+#Relaciones
+    
+    aprendiz = relationship("Aprendiz",
+                        back_populates="roles")
+    
+
+class Rol(Base):
+    __tablename__ = "Rol"
+    id_Rol = Column(Integer, 
+                primary_key=True)
+    nombre_Rol = Column(String(50))
+    estado_Rol = Column(Boolean)
+    descripcion_Rol = Column(String(200))
+    
+class Usuario(Base):
+    __tablename__ = "usuario"
+    id_Usuario = Column(Integer, 
+                primary_key=True)
+    nombre_Usuario = Column(String(50))
+    apellido_Usuario = Column(String(50))
+    email_Usuario = Column(String(60))
+    password_Usuario = Column(String(100))
+    fecha_Creacion = Column(Date)
+    habilidades_Usuario = Column(String(200))
+    trueques_Usuario = Column(Integer)
+    rol_UsuarioFK = Column(Integer,
+                ForeignKey("Rol.id_Rol"))
+    
+class Trueque(Base):
+    __tablename__ = "trueque"
+    id_Trueque = Column(Integer, 
+                primary_key=True)
+    fecha_Trueque = Column(Date)
+    estado_Trueque = Column(Boolean)
+    descripcion_Trueque = Column(String(200))
+    id_UsuariosFK = Column(Integer,
+                ForeignKey("usuario.id_Usuario"))
+    
+class Feedback(Base):
+    _tablename__ ="feedback"
+    id_Feedback = Column(Integer, 
+                primary_key=True)
+    fecha_Feedback = Column(Date)
+    descripcion_Feedback = Column(String(200))
+    calificacion_Feedback = Column(Float)
+    id_TruequeFK = Column(Integer,
+                ForeignKey("trueque.id_Trueque"))
+    id_UsuarioFK = Column(Integer,
+                ForeignKey("usuario.id_Usuario"))
+    
+class Mensaje(Base):
+    __tablename__ = "mensaje"
+    
